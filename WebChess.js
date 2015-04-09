4efcef24489b7b092237e9a8bb5a4f00391f6e88
Brenw <Brenw2@gmail.com>
2015-04-09 00:43:13 -0400

    cleaned the code a little and then made pawns able to be made on reapeat

--- WebChess.js
+++ WebChess.js
@@ -8,130 +8,149 @@ var mouseX = 0,
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-var bishop;
-
-var position = {x:.56, y: .03, z: .57};
-
-
-
 init();
 animate();
-
 
 function init() {
     container = document.createElement('div');
     document.body.appendChild(container);
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.z = 1;
-    camera.position.y = 3;
-    camera.position.x = 1;
+
+    //camera
+    camera();
+
     // scene
     scene = new THREE.Scene();
 
     //lights
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(2, 4, -3).normalize();
-    scene.add(directionalLight);
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(-2, 4, 0).normalize();
-    scene.add(directionalLight);
+    directionalLightLeft();
+    directionalLightRight();
 
     // models
     whiteBishop();
+    whitePawn(.56,.03,.80);
+    whitePawn(.56,.03,.57);
+    whitePawn(.56,.03,.34);
+    whitePawn(.56,.03,.11);
+    whitePawn(.56,.03,-.12);
+    whitePawn(.56,.03,-.36);
+    whitePawn(.56,.03,-.36);
+    whitePawn(.56,.03,-.59);
+    whitePawn(.56,.03,-.81);
     ChessBoard();
 
+    //renderer
+    renderer();
+
+    //Scene Controls
+    controls();
+
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function camera() {
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.z = 0;
+    camera.position.y = 10;
+    camera.position.x = 0;
+}
+
+function directionalLightLeft() {
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(2, 4, -3).normalize();
+    scene.add(directionalLight);
+}
+
+function directionalLightRight() {
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(-2, 4, 0).normalize();
+    scene.add(directionalLight);
+}
+
+function renderer() {
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
+}
 
+function controls() {
     controls = new THREE.TrackballControls(camera, renderer.domElement);
     controls.rotateSpeed = 0.5;
-
-    window.addEventListener('resize', onWindowResize, false);
 
 }
 
 function whiteBishop() {
-    var onProgress = function(xhr) {
+    var onProgress = function (xhr) {
         if (xhr.lengthComputable) {
             var percentComplete = xhr.loaded / xhr.total * 100;
             console.log(Math.round(percentComplete, 2) + '% downloaded');
         }
     };
 
-    var onError = function(xhr) {};
+    var onError = function (xhr) {};
 
 
     THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
     var loader = new THREE.OBJMTLLoader();
-    loader.load('chessPiece/WhiteBishop.obj', 'chessPiece/WhiteBishop.mtl', function(object) {
-        
-        object.position.x = .56;
+    loader.load('chessPiece/WhiteBishop.obj', 'chessPiece/WhiteBishop.mtl', function (object) {
+
+        object.position.x = .79;
         object.position.y = .03;
         object.position.z = .57;
         scene.add(object);
-        tween = new TWEEN.Tween(object.position).to({
-        x: .56,
-        y: .3,
-        z: .57
-    }, 3000).delay(1000);
-     tween.easing( TWEEN.Easing.Elastic.Out).start();
-    
-        object.position.x = .56;
-        object.position.y = .3;
-        object.position.z = .57;    
-        
-    tween2 = new TWEEN.Tween(object.position).to({
-        x: .06,
-        y: .3,
-        z: .57
-    }, 3000).delay(3000);
-     tween2.easing( TWEEN.Easing.Elastic.Out).start();
-        
-        object.position.x = .045;
-        object.position.y = .3;
-        object.position.z = .57;
-        
-        
-    tween3 = new TWEEN.Tween(object.position).to({
-        x: .1 ,
-        y: .027,
-        z: .54
-    }, 1000).delay(6000);
-     tween3.start();    
-        
-        object.position.x = .56;
-        object.position.y = .03;
-        object.position.z = .57
-        
+
+        tweeenUp(object);
+        tweenAcross(object);
+        tweenDown(object);
+
     }, onProgress, onError);
-    
-    
+
+
 }
 
-function ChessBoard() {
-    var onProgress = function(xhr) {
+function whitePawn(x,y,z) {
+    var onProgress = function (xhr) {
         if (xhr.lengthComputable) {
             var percentComplete = xhr.loaded / xhr.total * 100;
             console.log(Math.round(percentComplete, 2) + '% downloaded');
         }
     };
 
-    var onError = function(xhr) {};
+    var onError = function (xhr) {};
 
+    THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
+    var loader = new THREE.OBJMTLLoader();
+    loader.load('chessPiece/WhitePawn.obj', 'chessPiece/WhitePawn.mtl', function (object) {
+
+        object.position.x = x;//.56
+        object.position.y = y;//.03
+        object.position.z = z;//.57
+        scene.add(object);
+
+    }, onProgress, onError);
+
+
+}
+
+
+
+function ChessBoard() {
+    var onProgress = function (xhr) {
+        if (xhr.lengthComputable) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+        }
+    };
+    var onError = function (xhr) {};
 
     THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
 
     var loader = new THREE.OBJMTLLoader();
-    loader.load('chessPiece/Chessboard.obj', 'chessPiece/Chessboard.mtl', function(object) {
+    loader.load('chessPiece/Chessboard.obj', 'chessPiece/Chessboard.mtl', function (object) {
         object.position.x = 0;
         object.position.y = 0;
         scene.add(object);
-         
     }, onProgress, onError);
-
 }
 
 function onDocumentMouseMove(event) {
@@ -149,7 +168,6 @@ function onDocumentMouseMove(event) {
         camera.position.y = radious * Math.sin(phi * Math.PI / 360);
         camera.position.z = radious * Math.cos(theta * Math.PI / 360) * Math.cos(phi * Math.PI / 360);
         camera.updateMatrix();
-
     }
 
     mouse3D = projector.unprojectVector(
@@ -160,11 +178,50 @@ function onDocumentMouseMove(event) {
         camera
     );
     ray.direction = mouse3D.subSelf(camera.position).normalize();
-
     interact();
     render();
-
 }
+
+function tweeenUp(object) {
+    tween = new TWEEN.Tween(object.position).to({
+        x: .56,
+        y: .3,
+        z: .57
+    }, 3000).delay(1000);
+    tween.easing(TWEEN.Easing.Elastic.Out).start();
+
+    object.position.x = .56;
+    object.position.y = .3;
+    object.position.z = .56;
+}
+
+function tweenAcross(object) {
+    tween2 = new TWEEN.Tween(object.position).to({
+        x: .045,
+        y: .3,
+        z: .57
+    }, 3000).delay(3000);
+    tween2.easing(TWEEN.Easing.Elastic.Out).start();
+
+    object.position.x = .045;
+    object.position.y = .3;
+    object.position.z = .56;
+}
+
+function tweenDown(object) {
+    tween3 = new TWEEN.Tween(object.position).to({
+        x: .11,
+        y: .03,
+        z: .56
+    }, 1000).delay(6000);
+    tween3.start();
+
+    object.position.x = .56;
+    object.position.y = .03;
+    object.position.z = .56
+}
+
+
 
 function onWindowResize() {
 
@@ -175,10 +232,6 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function animUp(){
-    
 }
 
 //
